0ba8f327a52f45b8928463e70a094d9892cf320f
Fernando Alejandro Yerena Ramos <ferdinandalexandros@gmail.com>
2022-05-02 12:48:09 -0500

    feat: Improve responsive design

--- src/Components/File.jsx
+++ src/Components/File.jsx
@@ -18,9 +18,9 @@ function File ({ uuid, name, dataURL, gif, gifName, isTranscoded, isProcessing }
   }, [isTranscoded, isProcessing]);
 
   return (
-    <div className='flex flex-row items-center justify-between w-full px-6 py-4'>
-      <h3 className='inline-block w-full text-base text-neutral-400'>{!isTranscoded ? name : gifName}</h3>
-      <div className='flex flex-row items-center justify-end w-full gap-4'>
+    <div className='flex flex-row flex-wrap items-center justify-between w-full px-6 py-4'>
+      <h3 className='inline-block w-full text-base text-neutral-400 sm:w-max'>{!isTranscoded ? name : gifName}</h3>
+      <div className='flex flex-row-reverse items-center justify-end flex-grow gap-4 mt-4 sm:justify-end sm:flex-row sm:mt-0 sm:ml-4'>
         {
           enableDelete &&
             <button className='inline-block align-middle transition-colors rounded-full w-9 h-9' onClick={deleteFile}>
