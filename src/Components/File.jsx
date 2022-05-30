dc1a68e9549d28f8bdf7db290dca0a4c9ca79d3d
Fernando Alejandro Yerena Ramos <ferdinandalexandros@gmail.com>
2022-05-29 20:49:43 -0500

    feat: Transcoded file can be removed individually

--- src/Components/File.jsx
+++ src/Components/File.jsx
@@ -8,7 +8,6 @@ import FilesContext from '../Context/FilesContext';
 import TranscodeContext from '../Context/TranscodeContext';
 
 function File ({ uuid, name, extension, dataURL, gif }) {
-  const [enableDelete, setEnableDelete] = useState(true);
   const [enableTranscode, setEnableTranscode] = useState(true);
   const [isTranscoded, setTranscoded] = useState(false);
   const { files, setFiles } = useContext(FilesContext);
@@ -24,7 +23,6 @@ function File ({ uuid, name, extension, dataURL, gif }) {
     } else setEnableTranscode(true);
 
     if (gif) {
-      setEnableDelete(false);
       setTranscoded(true);
     }
   }, [isProcessing]);
@@ -33,12 +31,9 @@ function File ({ uuid, name, extension, dataURL, gif }) {
     <div className='flex flex-row flex-wrap items-center justify-between w-full px-6 py-4'>
       <h3 className='inline-block w-full text-base text-neutral-400 sm:w-max'>{!isTranscoded ? `${name}.${extension}` : `${name}.gif`}</h3>
       <div className='flex flex-row-reverse items-center justify-end flex-grow gap-4 mt-4 sm:justify-end sm:flex-row sm:mt-0 sm:ml-4'>
-        {
-          enableDelete &&
-            <button className='inline-block align-middle transition-colors rounded-full w-9 h-9' onClick={deleteFile}>
-              <IconDelete className='w-full h-full p-1 transition-colors rounded-full fill-neutral-700 bg-neutral-800 hover:fill-red-500 hover:bg-neutral-700' />
-            </button>
-        }
+        <button className='inline-block align-middle transition-colors rounded-full w-9 h-9' onClick={deleteFile}>
+          <IconDelete className='w-full h-full p-1 transition-colors rounded-full fill-neutral-700 bg-neutral-800 hover:fill-red-500 hover:bg-neutral-700' />
+        </button>
         {
           enableTranscode
             ? <Transcode uuid={uuid} filename={name} from={extension} gif={gif} objectURL={dataURL} />
