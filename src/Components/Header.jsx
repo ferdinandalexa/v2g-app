3093c8d80cf52560c5e88ceac4833c2a128e8cfe
Fernando Alejandro Yerena Ramos <ferdinandalexandros@gmail.com>
2022-05-31 18:00:15 -0500

    brand: Add logo like component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,15 @@
 import { Link } from 'wouter';
 
+import Logo from './Logo';
 import Info from './Info';
 
 function Header () {
   return (
     <header className='flex flex-row items-center justify-between px-6 py-4 text-center rounded-xl bg-neutral-800'>
       <Link to='/'>
-        <h1 className='text-lg font-bold cursor-pointer text-neutral-200'>V2G App</h1>
+        <a>
+          <Logo width='72px' className='transition-opacity cursor-pointer hover:opacity-50' />
+        </a>
       </Link>
       <Info />
     </header>
