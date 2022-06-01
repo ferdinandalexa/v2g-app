7e99ff7e4a5379e898886bbcbec85fe78cb7b706
Fernando Alejandro Yerena Ramos <ferdinandalexandros@gmail.com>
2022-05-31 19:00:48 -0500

    styles: Fix colors and svg paths

--- src/Components/ExtLink.jsx
+++ src/Components/ExtLink.jsx
@@ -1,6 +1,6 @@
 function ExtLink ({ url, children, type, className }) {
   return (
-    <a href={url} target='_blank' rel='noreferrer noopener' type={type} className={`text-slate-400 ${className}`}>{children}</a>
+    <a href={url} target='_blank' rel='noreferrer noopener' type={type} className={`text-indigo-300 opacity-80 ${className}`}>{children}</a>
   );
 }
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,7 +8,7 @@ function Header () {
     <header className='flex flex-row items-center justify-between px-6 py-4 text-center rounded-xl bg-neutral-800'>
       <Link to='/'>
         <a>
-          <Logo width='72px' className='transition-opacity cursor-pointer hover:opacity-50' />
+          <Logo width='96px' className='transition-opacity cursor-pointer hover:opacity-50' />
         </a>
       </Link>
       <Info />
