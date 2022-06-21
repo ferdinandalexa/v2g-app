18d19beb12ccf369f32914f0c5e0442338dddc9a
Fernando Alejandro Yerena Ramos <ferdinandalexandros@gmail.com>
2022-06-21 16:34:56 -0500

    :test_tube: test(logo): add test logo component

---
+++ src/Components/Logo.test.jsx
@@ -0,0 +1,26 @@
+import { describe, expect, it } from 'vitest';
+import { render, screen } from '../utilities/test-utils';
+import Logo from './Logo';
+
+describe('Logo display text', () => {
+  it('Render logo with default values', () => {
+    render(<Logo />);
+
+    expect(screen.getByRole('img')).toBeInTheDocument();
+    expect(screen.getByRole('img')).toHaveAttribute('viewBox');
+    // width and height default value: '100%'
+    expect(screen.getByRole('img')).toHaveAttribute('width', '100%');
+    expect(screen.getByRole('img')).toHaveAttribute('height', '100%');
+  });
+
+  it('Render logo with default values', () => {
+    const width = '50px';
+    const height = '70px';
+    render(<Logo width={width} height={height} />);
+
+    expect(screen.getByRole('img')).toBeInTheDocument();
+    expect(screen.getByRole('img')).toHaveAttribute('viewBox');
+    expect(screen.getByRole('img')).toHaveAttribute('width', width);
+    expect(screen.getByRole('img')).toHaveAttribute('height', height);
+  });
+});
