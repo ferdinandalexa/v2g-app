bbb9711544a98c36a8f29b744c7beee17ab80fe4
Fernando Alejandro Yerena Ramos <ferdinandalexandros@gmail.com>
2022-12-21 16:39:26 -0600

    :memo: refactor(footer): update footer links

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer () {
   return (
     <footer className='w-full px-6 py-4 text-center'>
       <p className='text-neutral-500'>Project made by
-        <ExtLink url='https://twitter.com/_ferdinandalexa'> @_ferdinandalexa</ExtLink>
+        <ExtLink url='https://github.com/ferdinandalexa/v2g-app'> @_ferdinandalexa</ExtLink>
       </p>
     </footer>
   );
